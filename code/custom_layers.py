f4c85a85d3ba8bddfb4240c8cc4546f9c9600687
Erick <erick.galinkin@gmail.com>
2019-11-08 10:47:25 -0800

    Add wavelet and fourier model. Custom layers

---
+++ code/custom_layers.py
@@ -0,0 +1,87 @@
+from tensorflow.keras.layers import Layer
+import tensorflow as tf
+import numpy as np
+import pywt
+
+
+class FourierConvLayer(Layer):
+    def __init__(self, output_dim, **kwargs):
+        super(FourierConvLayer, self).__init__()
+
+        self.output_dim = output_dim
+        self.fft = tf.signal.fft
+        self.ifft = tf.signal.ifft
+        self.kernel = None  # We initialize the kernel below - there is no way to initialize without an input shape.
+
+        self.conv1d = tf.nn.conv1d
+        self.relu = tf.nn.relu
+
+    def build(self, input_shape):
+        self.kernel = self.add_weight(name='kernel',
+                                      shape=(input_shape[-1], self.output_dim),
+                                      initializer='uniform',
+                                      trainable=True)
+        super(FourierConvLayer, self).build(input_shape)
+
+    def call(self, x):
+        x = tf.cast(x, dtype=tf.dtypes.complex64)
+        y = tf.cast(self.kernel, dtype=tf.dtypes.complex64)
+        fourier_x = self.fft(x)
+        fourier_x = tf.matmul(fourier_x, y)
+        x = self.ifft(fourier_x)
+        x = tf.convert_to_tensor(x)
+        x = tf.cast(x, dtype=tf.float32)
+        return x
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0], self.output_dim)
+
+
+class WaveletConvLayer(Layer):
+    def __init__(self, output_dim, **kwargs):
+        super(WaveletConvLayer, self).__init__()
+
+        self.output_dim = output_dim
+        self.dwt = pywt.dwtn
+        self.idwt = pywt.idwtn
+        self.kernel = None  # We initialize the kernel below - there is no way to initialize without an input shape.
+
+        self.conv1d = tf.nn.conv1d
+        self.relu = tf.nn.relu
+
+    def build(self, input_shape):
+        self.kernel = self.add_weight(name='kernel',
+                                      shape=(input_shape[1], self.output_dim),
+                                      initializer='uniform',
+                                      trainable=True)
+        super(WaveletConvLayer, self).build(input_shape)
+
+    def call(self, x):
+        with tf.compat.v1.Session():
+            a = x.eval(feed_dict={})
+        print(type(a))
+        exit()
+        a = np.ones(x.shape[1])
+        b = a * x[0]
+        y = tf.cast(self.kernel, dtype=tf.dtypes.complex64)
+        wavelet_x = self.dwt(b, 'haar', axes=0)
+        wavelet_x = tf.matmul(wavelet_x, y)
+        x = self.idwt(wavelet_x)
+        x = tf.convert_to_tensor(x)
+        x = tf.cast(x, dtype=tf.float32)
+        return x
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0], self.output_dim)
+
+
+if __name__ == "__main__":
+    from tensorflow.keras.models import Model
+    from tensorflow.keras.layers import Input
+
+    print("Running test...")
+    inp = Input(shape=(100))
+    out = WaveletConvLayer(64)(inp)
+    model = Model(inp, out)
+
+    model.predict(tf.ones((1, 100)))
--- code/prototype_model.py
+++ code/prototype_model.py
@@ -5,23 +5,15 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
 import pywt
-
+from custom_layers import FourierConvLayer
 
 # Configuration options
-BATCH_SIZE = 99
+BATCH_SIZE = 100
 EPOCHS = 30
 
 # Data locations
 REQUEST_REPLY_CSV_LOCATION = "./data/requestreply.csv"
 REPLY_REPLY_CSV_LOCATION = "./data/replyreply.csv"
-
-
-class FourierConvLayer(layers.Layer):
-    pass
-
-
-class WaveletConvLayer(layers.Layer):
-    pass
 
 
 def df_to_dataset(X, y, shuffle=True, batch_size=32):
@@ -35,7 +27,7 @@ def df_to_dataset(X, y, shuffle=True, batch_size=32):
 def fix_data(dataset, shuffle=True, batch_size=32):
     dataset = dataset.copy()
     labels = dataset.pop("Malicious")
-    X_train, X_test, y_train, y_test = train_test_split(dataset, labels, test_size=.2, stratify=labels)
+    X_train, X_test, y_train, y_test = train_test_split(dataset, labels, test_size=.25, stratify=labels)
 
     train = df_to_dataset(X_train, y_train, shuffle=shuffle, batch_size=batch_size)
     test = df_to_dataset(X_test, y_test, shuffle=shuffle, batch_size=batch_size)
@@ -48,6 +40,7 @@ def build_fc_model(features):
 
     model = Sequential()
     model.add(feature_layer)
+    model.add(layers.Dense(256, activation='relu'))
     model.add(layers.Dense(128, activation='relu'))
     model.add(layers.Dense(64, activation='relu'))
     model.add(layers.Dense(32, activation='relu'))
@@ -68,11 +61,12 @@ def build_conv_model(features):
     model = Sequential()
     model.add(feature_layer)
     model.add(layers.Reshape((100, 1)))
-    model.add(layers.Conv1D(128, input_shape=(None, 100), kernel_size=2, strides=1, activation='relu'))
+    model.add(layers.Conv1D(128, input_shape=(None, 100), kernel_size=5, strides=1, activation='relu'))
     model.add(layers.MaxPooling1D(pool_size=2, strides=1, padding='valid'))
-    model.add(layers.Conv1D(128, input_shape=(None, 100), kernel_size=5, strides=3, activation='relu'))
+    model.add(layers.Conv1D(128, input_shape=(None, 100), kernel_size=3, strides=2, activation='relu'))
+    model.add(layers.MaxPooling1D(pool_size=2, strides=None, padding='valid'))
+    model.add(layers.Conv1D(128, input_shape=(None, 100), kernel_size=2, strides=1, activation='relu'))
     model.add(layers.MaxPooling1D(pool_size=3, strides=None, padding='valid'))
-    model.add(layers.Conv1D(128, input_shape=(None, 100), kernel_size=3, strides=1, activation='relu'))
     model.add(layers.BatchNormalization())
     model.add(layers.Flatten())
     model.add(layers.Dense(128, activation='relu'))
@@ -89,7 +83,30 @@ def build_conv_model(features):
 
 
 def fourier_model(features):
-    pass
+    feature_layer = layers.DenseFeatures(features)
+
+    model = Sequential()
+    model.add(feature_layer)
+    model.add(layers.Reshape((100, 1)))
+    model.add(FourierConvLayer(128, autocast=False))
+    model.add(layers.MaxPooling1D(pool_size=2, strides=1, padding='valid'))
+    model.add(FourierConvLayer(64, autocast=False))
+    model.add(layers.MaxPooling1D(pool_size=2, strides=1, padding='valid'))
+    model.add(FourierConvLayer(32, autocast=False))
+    model.add(layers.MaxPooling1D(pool_size=2, strides=1, padding='valid'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Flatten())
+    model.add(layers.Dense(128, activation='relu'))
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(1, activation='sigmoid'))
+
+    model.compile(
+        optimizer='SGD',
+        loss='binary_crossentropy',
+        metrics=['accuracy']
+    )
+
+    return model
 
 
 def wavelet_model(features):
@@ -155,8 +172,8 @@ reply_reply_wavelet = create_wavelet_dataset(reply_reply_df)
 
 
 if __name__ == "__main__":
-    print("Training 1D convolutional model on request reply data")
+    print("Training 1D FCNN model on request reply data")
     features = generate_features(request_reply_df)
-    model = build_conv_model(features)
-    conv_qr_history, conv_qr_results = train_model(model, request_reply_df)
+    model = fourier_model(features)
+    fourier_qr_history, fourier_qr_results = train_model(model, request_reply_df)
 
